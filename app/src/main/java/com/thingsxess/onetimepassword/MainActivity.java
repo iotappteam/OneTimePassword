81edab0601c7d9f815b0f2665e3c48ae00fc4daf
Mashood <iotappteam@gmail.com>
2021-04-09 00:12:45 -0700

    some changes

--- app/src/main/java/com/thingsxess/onetimepassword/MainActivity.java
+++ app/src/main/java/com/thingsxess/onetimepassword/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "naya code", Toast.LENGTH_SHORT).show();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         //Initializing the instance
